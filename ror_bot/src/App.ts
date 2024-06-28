aed9f72c5ef3cc025df52f26c252f8744079b692
Capibara <csjc1111@gmail.com>
2024-06-28 15:53:10 +0800

    payable

--- ror_bot/src/App.ts
+++ ror_bot/src/App.ts
@@ -1,17 +1,32 @@
-import ethers from 'ethers';
+import {ethers} from 'ethers';
+import dotenv from "dotenv";
+import * as fs from "node:fs";
 
-async  function make_signature() {
+dotenv.config();
+
+const UniversalRouterABI = JSON.parse(fs.readFileSync(__dirname + "/../abi/UniversalRouter.json").toString());
+
+async function MakeSignature() {
+
+  if (!process.env.PRIVATE_KEY) {
+    throw new Error('PRIVATE_KEY environment variable is not set.');
+  }
+
+  const provider = new ethers.JsonRpcProvider("");
   // 1. 获取用户的钱包实例
-  const userWallet = new ethers.Wallet('YOUR_PRIVATE_KEY', provider);
+  const userWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   // 2. 计算 permit 数据的 digest
   const owner = '0x0123456789012345678901234567890123456789';
   const spender = '0x9876543210987654321098765432109876543210';
   const value = ethers.parseEther('1.0');
   const deadline = Math.floor(Date.now() / 1000) + 3600; // 1 小时后过期
+  const contract = new ethers.Contract("", UniversalRouterABI, userWallet);
   const nonce = await contract.nonces(owner);
   const digest = await contract.getPermitDigest(owner, spender, value, nonce, deadline);
   // 3. 使用用户的私钥对 digest 进行签名
-  const signingKey = new ethers.utils.SigningKey(userWallet.privateKey);
-  const signature = await signingKey.signDigest(digest);
+  const signingKey = new ethers.SigningKey(userWallet.privateKey);
+  const signature = await signingKey.sign(digest);
   console.log("signature", signature);
 }
+
+MakeSignature();
