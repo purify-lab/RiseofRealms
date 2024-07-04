85f99573260252c6ba2de9f0f61bdc04e7b2e4d4
alrightlook <alrightlook@gmail.com>
2024-07-04 17:17:42 +0800

    add Capital model

--- templates/realms/packages/client/Assets/Scripts/MUDComponents/PlayerDetailComponent.cs
+++ templates/realms/packages/client/Assets/Scripts/MUDComponents/PlayerDetailComponent.cs
@@ -9,6 +9,8 @@ public class PlayerDetailComponent : MUDComponent
     protected override void UpdateComponent(MUDTable table, UpdateInfo info)
     {
         PlayerDetailTable update = (PlayerDetailTable)table;
-        Debug.Log(">>> On Update PlayerDetailComponent Type: " + info.UpdateType);
+        Debug.Log(">>> PDT : " + info.UpdateType);
+        Debug.Log($" >>>> PDT: + {update.CavalryA} :::  {update.Infantry}");
+        var soldier = update.CavalryA + update.CavalryB + update.CavalryC + update.Infantry;
     }
 }
